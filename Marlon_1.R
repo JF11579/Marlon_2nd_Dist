a56dff6f265df5d72bb1cebdb4b955dc8f9cd2cf
Joe Foley <jfoley648@gmail.com>
2019-02-28 15:08:40 -0500

    second

--- Marlon_1.R
+++ Marlon_1.R
@@ -4,5 +4,32 @@ library(ggplot2)
 
 # Data
 NY_second<- read.csv("NY_2nd_race.csv")
-#MyData <- read.csv(file="TheDataIWantToReadIn.csv", header=TRUE, sep=",")
-NY_second<- read.csv("NY_2nd_race.csv",header = TRUE,sep = " '")
+dim(NY_second)
+head(NY_second)
+#First aggregate per year
+#https://cyberhelp.sesync.org/data-manipulation-in-R-lesson/2016/08/25/
+#https://datacarpentry.org/R-genomics/04-dplyr.html
+by_year<- NY_second%>%
+  group_by(Year)%>%
+  summarize(sum= sum(pop_5_yrs_and_over))
+by_year
+#
+two<- NY_second%>%group_by(Year)%>%
+      summarize(total=sum(pop_5_yrs_and_over))
+two
+#
+three<- NY_second%>%group_by(Year)%>%
+  summarize(total=sum(pop_5_yrs_and_over))
+#
+stocks <- tibble(
+  time = as.Date('2009-01-01') + 0:9,
+  X = rnorm(10, 0, 1),
+  Y = rnorm(10, 0, 2),
+  Z = rnorm(10, 0, 4)
+)
+stocks
+#
+gather(stocks, "stock", "price", -time)
+#
+four<- NY_second%>% gather("Race","Pop",African_American:White)
+four
